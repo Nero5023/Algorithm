d71c4c5217f0a6e29027fb6a3f7314e58328466d
Nero Zuo <502350123@qq.com>
2017-06-24 00:14:55 +0800

    no message

--- 290_Word_Pattern.py
+++ 290_Word_Pattern.py
@@ -5,4 +5,18 @@ class Solution(object):
         :type str: str
         :rtype: bool
         """
-        
+        dic = {}
+        dic2 = {}
+        words = str.split(" ")
+        if len(words) != len(pattern):
+            return False
+        for (index, x) in enumerate(pattern):
+            if x in dic:
+                if dic[x] != words[index]:
+                    return False
+            else:
+                if words[index] in dic2:
+                    return False
+                dic2[words[index]] = x
+                dic[x] = words[index]
+        return True
