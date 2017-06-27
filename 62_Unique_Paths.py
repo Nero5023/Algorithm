9c280505f7b83905ac06e9a0bf43356eeb386552
Nero Zuo <502350123@qq.com>
2017-06-27 22:45:30 +0800

    62

---
+++ 62_Unique_Paths.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m+n-2 == 1:
+            return 1
+        return C(m+n-2, n-1)
+
+def factorial(n):
+    res = reduce(lambda acc, x: acc*x, range(1, 1+n), 1)
+    return res
+
+
+def C(n,m):
+    return factorial(n)/(factorial(m)*factorial(n-m))
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.uniquePaths(1,10)
